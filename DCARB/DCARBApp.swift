02ca804901a069836562ede3c16aa006af19bf8b
sidereior <alexanderlnanda@gmail.com>
2023-10-14 11:21:19 -0400

    first commit

---
+++ DCARB/DCARBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DCARBApp.swift
+//  DCARB
+//
+//  Created by Alexander Nanda on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DCARBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
